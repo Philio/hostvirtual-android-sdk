f95888f13922ffaa1768ac17a738412cd7224433
Phil Bayfield <phil@bayfmail.com>
2013-05-14 14:31:22 -0700

    added additional methods

--- src/org/vr/Cloud.java
+++ src/org/vr/Cloud.java
@@ -14,7 +14,7 @@ public class Cloud extends AbstractAPI {
 
 	// Cloud URI path
 	private static final String CLOUD_URI = BASE_URI + "cloud/";
-	
+
 	/**
 	 * Set request object and API key
 	 * 
@@ -23,62 +23,6 @@ public class Cloud extends AbstractAPI {
 	 */
 	public Cloud(AbstractRequest request, String apiKey) {
 		super(request, apiKey);
-	}
-
-	/**
-	 * Get details for all servers on the account
-	 * 
-	 * @return
-	 * @throws APIException
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	public JSONArray servers() throws APIException, IOException, JSONException {
-		List<NameValuePair> params = getInitialParams();
-		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "servers", params));
-	}
-
-	/**
-	 * Get all server options for a specific server
-	 * 
-	 * @param serverId
-	 * @return
-	 * @throws APIException
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	public JSONObject server(int serverId) throws APIException, IOException, JSONException {
-		List<NameValuePair> params = getInitialParams();
-		return new JSONObject(mRequest.execute(Method.GET, CLOUD_URI + "server/" + Integer.toString(serverId),
-				params));
-	}
-
-	/**
-	 * Get summary information for a specific server
-	 * 
-	 * @param serverId
-	 * @return
-	 * @throws APIException
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	public JSONObject serverSummary(int serverId) throws APIException, IOException, JSONException {
-		List<NameValuePair> params = getInitialParams();
-		return new JSONObject(mRequest.execute(Method.GET,
-				CLOUD_URI + "serversummary/" + Integer.toString(serverId), params));
-	}
-
-	/**
-	 * Get available OS images
-	 * 
-	 * @return
-	 * @throws APIException
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	public JSONArray osList() throws APIException, IOException, JSONException {
-		List<NameValuePair> params = getInitialParams();
-		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "images", params));
 	}
 
 	/**
@@ -138,7 +82,20 @@ public class Cloud extends AbstractAPI {
 	}
 
 	/**
-	 * Get available packages
+	 * Get available OS images
+	 * 
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray osList() throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "images", params));
+	}
+
+	/**
+	 * Get current packages (includes details of unprovisioned/purchased servers)
 	 * 
 	 * @return
 	 * @throws APIException
@@ -148,6 +105,183 @@ public class Cloud extends AbstractAPI {
 	public JSONArray packages() throws APIException, IOException, JSONException {
 		List<NameValuePair> params = getInitialParams();
 		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "packages", params));
+	}
+
+	/**
+	 * Get details of a single package
+	 * 
+	 * @param packageId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject packages(int packageId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(
+				mRequest.execute(Method.GET, CLOUD_URI + "package/" + Integer.toString(packageId), params));
+	}
+
+	/**
+	 * Get details for all servers on the account
+	 * 
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray servers() throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "servers", params));
+	}
+
+	/**
+	 * Get all server options for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject serverOptions(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(mRequest.execute(Method.GET, CLOUD_URI + "server/" + Integer.toString(serverId), params));
+	}
+
+	/**
+	 * Get summary information for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject serverSummary(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(mRequest.execute(Method.GET, CLOUD_URI + "serversummary/" + Integer.toString(serverId),
+				params));
+	}
+
+	/**
+	 * Get monthly bandwidth summary for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray bandwidth(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "servermonthlybw/" + Integer.toString(serverId),
+				params));
+	}
+
+	/**
+	 * Get IP addresses (IPv4 + IPv6) for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray ips(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "networkips/" + Integer.toString(serverId),
+				params));
+	}
+
+	/**
+	 * Get IPv4 addresses for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray ipv4(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "IPv4/" + Integer.toString(serverId), params));
+	}
+
+	/**
+	 * Get IPv6 addresses for a specific server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONArray ipv6(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONArray(mRequest.execute(Method.GET, CLOUD_URI + "IPv6/" + Integer.toString(serverId), params));
+	}
+
+	/**
+	 * Start rescue mode for a server
+	 * 
+	 * @param serverId
+	 * @param password
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject startRescue(int serverId, String password) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		params.add(new BasicNameValuePair("rescue_pass", password));
+		return new JSONObject(mRequest.execute(Method.POST,
+				CLOUD_URI + "server/start_rescue/" + Integer.toString(serverId), params));
+	}
+
+	/**
+	 * Stop rescue mode for a server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject stopRescue(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(mRequest.execute(Method.POST,
+				CLOUD_URI + "server/stop_rescue/" + Integer.toString(serverId), params));
+	}
+
+	/**
+	 * Start a server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject start(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(mRequest.execute(Method.POST, CLOUD_URI + "server/start/" + Integer.toString(serverId),
+				params));
+	}
+
+	/**
+	 * Stop a server
+	 * 
+	 * @param serverId
+	 * @return
+	 * @throws APIException
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public JSONObject stop(int serverId) throws APIException, IOException, JSONException {
+		List<NameValuePair> params = getInitialParams();
+		return new JSONObject(mRequest.execute(Method.POST, CLOUD_URI + "server/shutdown/" + Integer.toString(serverId),
+				params));
 	}
 
 }
